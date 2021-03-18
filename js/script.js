c2a1c44e8894f40ea2fb827b914da8808e942838
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-03-18 12:24:43 +0300

    add axios library

--- js/script.js
+++ js/script.js
@@ -197,9 +197,22 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    // data.forEach(({
+    //     img,
+    //     altimg,
+    //     title,
+    //     descr,
+    //     price
+    // }) => {
+    //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    // });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({
+            data.data.forEach(({
                 img,
                 altimg,
                 title,
@@ -318,9 +331,4 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-
-    // fetch('http://localhost:3000/menu')
-    //     .then(data => data.json())
-    //     .then(res => console.log(res));
-
 });
