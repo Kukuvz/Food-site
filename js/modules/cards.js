d5f5fa4f3b228032a9dc8ae7a2b126b8a7668206
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-03-21 14:56:54 +0300

    working with modules&services

--- js/modules/cards.js
+++ js/modules/cards.js
@@ -1,3 +1,7 @@
+import {
+    getResource
+} from '../services/services';
+
 function cards() {
     // Class
 
@@ -43,16 +47,6 @@ function cards() {
         }
     }
 
-    const getResource = async (url) => {
-        const res = await fetch(url);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
-        return await res.json();
-    };
-
     getResource('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({
@@ -81,4 +75,4 @@ function cards() {
     //     });
 }
 
-module.exports = cards;
+export default cards;
